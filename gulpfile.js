46991420ba23701b559b4bd360102e089d4a0c8b
Alexandr <mr.l.v.alexandre@gmail.com>
2019-09-20 14:25:09 +0600

    experemental

--- gulpfile.js
+++ gulpfile.js
@@ -5,81 +5,194 @@ const cleanCSS = require('gulp-clean-css');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
 const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const tsify = require('tsify');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
 const tsProject = ts.createProject('tsconfig.json');
 const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
+const babelify = require('babelify');
+const size = require('gulp-filesize');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const rimraf = require('rimraf');
+const gulpIf = require('gulp-if');
 
+const config = {
+    env: 'development'
+}
 // Paths
-const output = {
-    js: './dist/js',
-    css: './dist/css',
-    assets: './dist/assets',
-    html: './dist/pages',
+const path = {
+    input: {
+        jsDesk: 'src/entries/desktop/index.ts',
+        jsMob: 'src/entries/mobile/index.ts',
+        css: ['src/entries/**/*.scss', '!src/entries/**/_*.scss'],
+        image: 'src/images/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+        html: ['./src/*.pug', './src/pages/**/*.pug', '!./src/pages/**/_*.pug'],
+    },
+    output: {
+        jsDesk: 'dist/js/desktop',
+        jsMob: 'dist/js/mobile',
+        css: 'dist/css',
+        image: 'dist/images',
+        fonts: 'dist/fonts',
+        html: 'dist',
+    }
 };
 
+function image() {
+    return gulp.src(path.input.image) //Выберем наши картинки
+        .pipe(gulpIf(isProd(), imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        })))
+        .pipe(gulp.dest((file) => {
+            const pathFromSrc = file.path.replace(/(.*?assets.*src.*images)/, '').replace(/(?:.*\\)+([^\\]+)/, '');
+            return `${path.output.image}${pathFromSrc}`;
+        }))
+        .pipe(browserSync.stream());
+    // done();
+}
+
+function fonts() {
+    return gulp.src(path.input.fonts)
+    // .pipe(gulp.dest(path.build.fonts)
+        .pipe(gulp.dest((file) => {
+            const pathFromSrc = file.path.replace(/(.*?assets.*src.*fonts)/, '').replace(/(?:.*\\)+([^\\]+)/, '');
+            return `${path.output.fonts}${pathFromSrc}`
+        }));
+}
+
 function style() {
-    return gulp.src('./src/*.scss')
+    return gulp.src(path.input.css)
+        .pipe(sass({
+            includePaths: ['node_modules']
+        }))
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             cascade: false
         }))
         .pipe(cleanCSS(
-        {
+            {
                 debug: true,
                 compatibility: 'ie9'
-            },
-        (details) => {
-                console.log(`Original file   - ${details.name}: ${details.stats.originalSize}`);
-                console.log(`Compressed file - ${details.name}: ${details.stats.minifiedSize}`);
             }
         ))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(output.css))
-        .pipe(browserSync.stream())
+        .pipe(gulp.dest((file) => {
+            const pathFromSrc = file.base.replace(/(.*?assets.*src.*entries)/, '');
+            return `${path.output.css}${pathFromSrc}`
+        }))
+        .pipe(size())
+        .pipe(browserSync.stream());
 }
 
 function html() {
-    return gulp.src(['./src/*.pug', './src/pages/**/*.pug', '!./src/pages/**/_*.pug'])
+    return gulp.src(path.input.html)
         .pipe(pug({
             pretty: true,
         }))
-        .pipe(gulp.dest( (file) => {
-            const pathFromSrc = file.base.replace(/(.*?src)/, '');
-            return `./dist${pathFromSrc}`
+        .pipe(gulp.dest((file) => {
+            const pathFromSrc = file.base.replace(/(.*?assets.*src)/, '');
+            return `${path.output.html}${pathFromSrc}`
         }))
-        .pipe(browserSync.stream())
-}
-    
-function js() {
-    // return tsProject.src()
-    // .pipe(tsProject())
-    // .pipe(gulp.dest('./dist/scripts'))
-    // .pipe(browserSync.stream())
-    return tsProject.src()
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .pipe(babel())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/scripts'))
-    .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
 }
 
-function watch() {
-    html();
-    style();
-    js();
+function jsDesktop() {
+    return browserify({
+        basedir: '.',
+        debug: !isProd,
+        entries: path.input.jsDesk,
+        cache: {},
+        packageCache: {},
+        extensions: ['.ts', '.js']
+    })
+        .plugin(tsify)
+        .transform('babelify', {
+            presets: ['@babel/preset-env'],
+            extensions: ['.ts', '.js']
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulpIf(!isProd(), sourcemaps.init({loadMaps: true})))
+        .pipe(gulpIf(isProd(), uglify()))
+        .pipe(gulpIf(!isProd(), sourcemaps.write('./')))
+        .pipe(gulp.dest(path.output.jsDesk))
+        .pipe(size('asdsad'));
+}
+
+function jsMobile() {
+    return browserify({
+        // basedir: '.',
+        debug: !isProd,
+        entries: path.input.jsMob,
+        // cache: {},
+        // packageCache: {}
+    })
+        .plugin(tsify)
+        .transform('babelify', {
+            presets: ['@babel/preset-env'],
+            extensions: ['.ts']
+        })
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gulpIf(!isProd(), sourcemaps.init({loadMaps: true})))
+        .pipe(gulpIf(isProd(), uglify()))
+        .pipe(gulpIf(!isProd(), sourcemaps.write('./')))
+        .pipe(gulp.dest(path.output.jsMob))
+        .pipe(size());
+}
+
+function clear(done) {
+    return rimraf('dist', done);
+}
+
+gulp.task('js', gulp.parallel(jsDesktop, jsMobile));
+gulp.task('style', style);
+gulp.task('html', html);
+gulp.task('image', image);
+gulp.task('fonts', fonts);
+
+gulp.task('build', gulp.series(clear, gulp.parallel(style, 'js', html, image, fonts)));
+
+gulp.task('liveServer', () => {
     browserSync.init({
         server: {
-            baseDir: 'dist'
+            baseDir: 'dist',
+            index: 'pages/index.html'
         }
     });
     gulp.watch('./src/**/*.pug', html);
     gulp.watch('./src/**/*.scss', style);
-    gulp.watch('./src/**/*.ts', js);
+    gulp.watch('./src/**/*.ts', gulp.parallel(jsDesktop, jsMobile));
+    gulp.watch('./src/images/**/*.*', image);
+    gulp.watch('./src/fonts/**/*.*', fonts);
+});
+
+gulp.task('watch', gulp.series('build', 'liveServer'));
+
+function isProd() {
+    return config.env === 'production';
 }
 
-exports.style = style;
-exports.html = html;
-exports.js = js;
-exports.watch = watch;
+function setProdEnv(done) {
+    process.env.NODE_ENV = config.env = 'production';
+    done()
+}
+
+function setDevEnv(done) {
+    process.env.NODE_ENV = config.env = 'development';
+    done()
+}
+
+gulp.task('dev', gulp.series(setDevEnv, 'build'));
+gulp.task('prod', gulp.series(setProdEnv, 'build'));
